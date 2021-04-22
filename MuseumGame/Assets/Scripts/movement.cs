b5f0e21a9e9d26fde5d49bc052efffc395b78b52
thoopchuk26 <thoopchuk@gmail.com>
2021-04-22 10:47:17 -0500

    Fixed Jumps Again

--- MuseumGame/Assets/Scripts/movement.cs
+++ MuseumGame/Assets/Scripts/movement.cs
@@ -36,9 +36,10 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (rb2d.velocity.y == 0)
+        if (rb2d.velocity.y < 0.1f && rb2d.velocity.y > -0.1f)
         {
             isGrounded = true;
+            numJumps = maxJumps;
         }
 
         if (Input.GetKeyDown("space") && numJumps > 0)
