7d6a75a7cadb30ee6b87f001f963937f8ce6ba46
Mars Paton <pumpkinpotato5@gmail.com>
2021-04-22 11:21:53 -0500

    Added collision for Fleurite enemy
    The Fleurite enemy now does its idle animation, has two animation controllers total, and can be jumped on and destroyed.

--- MuseumGame/Assets/Scripts/enemyBehaviorFleurite.cs
+++ MuseumGame/Assets/Scripts/enemyBehaviorFleurite.cs
@@ -2,21 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyBehFleurite : MonoBehaviour
+public class enemyBehaviorFleurite : MonoBehaviour
 {
     public int currentHealth;
     public int maxHP = 10;
+
     void Start()
     {
         currentHealth = maxHP;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (currentHealth <= 0)
         {
-            
+            Object.Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D coll = collision.collider;
+
+        if (coll.name == "Player")
+        {
+            Vector3 conPoint = collision.contacts[0].point;
+            Vector3 center = coll.bounds.center;
+
+            bool top = Mathf.Abs(conPoint.y) > Mathf.Abs(center.y);
+            bool sideCheck = (conPoint.x > center.x - 0.1) && (conPoint.x < center.x + 0.1);
+            if (top && sideCheck)
+            {
+                currentHealth = 0;
+            }
         }
     }
 }
