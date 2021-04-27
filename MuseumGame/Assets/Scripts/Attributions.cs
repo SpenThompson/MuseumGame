7d6d1f5ee707acfc549c7f637b4ef98029ba26ab
Spencer <james.spencer.thompson@gmail.com>
2021-04-26 19:21:56 -0500

    Fonts Added and Assigned

---
+++ MuseumGame/Assets/Scripts/Attributions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributions : MonoBehaviour
+{
+    /* Powerup Font: https://www.1001freefonts.com/karmafuture.font
+     * Menu Font: https://www.1001freefonts.com/roses-are-ff0000.font
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
