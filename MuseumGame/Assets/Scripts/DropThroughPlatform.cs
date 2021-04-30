b88ec036395259f93297394198e2be70aef2f038
Ivy <ivyjs@hendrix.edu>
2021-04-30 17:31:17 -0500

    Added one way platforms

---
+++ MuseumGame/Assets/Scripts/DropThroughPlatform.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropThroughPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxisRaw("Vertical"));
+    }
+
+    //https://forum.unity.com/threads/jumping-down-through-a-platform.414902/
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player" && (Input.GetAxisRaw("Vertical") < 0)){
+            TogglePlatform();
+            Invoke("TogglePlatform", 0.5f);
+        }
+    }
+    public void TogglePlatform()
+    {
+        gameObject.GetComponent<Collider2D>().enabled = !gameObject.GetComponent<Collider2D>().enabled;
+    }
+}
+
