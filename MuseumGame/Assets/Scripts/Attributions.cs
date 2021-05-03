cd3c0289a496f4583c8fe24b00345f29c83369bb
Ivy <ivyjs@hendrix.edu>
2021-05-03 12:45:43 -0500

    Started work on updated dog level spawn with new tileset

--- MuseumGame/Assets/Scripts/Attributions.cs
+++ MuseumGame/Assets/Scripts/Attributions.cs
@@ -6,6 +6,8 @@ public class Attributions : MonoBehaviour
 {
     /* Powerup Font: https://www.1001freefonts.com/karmafuture.font
      * Menu Font: https://www.1001freefonts.com/roses-are-ff0000.font
+     * Sprites https://opengameart.org/content/minimalist-pixel-tileset
+     * https://opengameart.org/content/abandonauts-8x8-tile-assets
     */
     // Start is called before the first frame update
     void Start()
