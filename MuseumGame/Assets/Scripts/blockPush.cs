3e7f51504ee4f9207336a973af8939b872b55349
thoopchuk26 <thoopchuk@gmail.com>
2021-05-04 02:19:53 -0500

    Block Push Powerup Logic Finished

--- MuseumGame/Assets/Scripts/blockPush.cs
+++ MuseumGame/Assets/Scripts/blockPush.cs
@@ -6,17 +6,14 @@ public class blockPush : MonoBehaviour
 {
     public bool canPush = false;
 
-    private Rigidbody2D rb2d;
     private Rigidbody2D blockrb2d;
 
     public float pushPower = 2.0f;
-    public float weight = 6.0f;
-    private Vector2 force;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -25,13 +22,20 @@ public class blockPush : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.tag == "block" && canPush)
+        if(collision.collider.tag == "Block" && canPush)
         {
+            Debug.Log("pushing block");
             blockrb2d = collision.collider.GetComponent<Rigidbody2D>();
-            force = blockrb2d.velocity * pushPower;
-            rb2d.AddForceAtPosition(force, collision.GetContact(0).point);
+            if (GetComponent<movement>().faceRight)
+            {
+                blockrb2d.MovePosition(new Vector2(blockrb2d.position.x + pushPower/100, blockrb2d.position.y));
+            }
+            else if (!GetComponent<movement>().faceRight)
+            {
+                blockrb2d.MovePosition(new Vector2(blockrb2d.position.x - pushPower/100, blockrb2d.position.y));
+            }
         }
     }
 }
